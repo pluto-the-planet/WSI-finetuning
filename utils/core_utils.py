c8a38484c9f797d0b42e715e5c6c9d4e062f24ba
pluto-the-planet <86825641+pluto-the-planet@users.noreply.github.com>
2024-06-11 15:53:04 +0500

    core_utils.py

--- utils/core_utils.py
+++ utils/core_utils.py
@@ -131,7 +131,8 @@ def train(datasets, cur, args):
 
     print('\nInit loss function...', end=' ')
     if args.bag_loss == 'svm':
-        from topk.svm import SmoothTop1SVM
+        #from topk.svm import SmoothTop1SVM
+        from svm import SmoothTop1SVM
         loss_fn = SmoothTop1SVM(n_classes = args.n_classes)
         if device.type == 'cuda':
             loss_fn = loss_fn.cuda()
@@ -155,7 +156,8 @@ def train(datasets, cur, args):
             model_dict.update({'k_sample': args.B})
         
         if args.inst_loss == 'svm':
-            from topk.svm import SmoothTop1SVM
+            #from topk.svm import SmoothTop1SVM
+            from svm import SmoothTop1SVM
             instance_loss_fn = SmoothTop1SVM(n_classes = 2)
             if device.type == 'cuda':
                 instance_loss_fn = instance_loss_fn.cuda()
@@ -268,7 +270,8 @@ def test(datasets, cur, args):
 
     print('\nInit loss function...', end=' ')
     if args.bag_loss == 'svm':
-        from topk.svm import SmoothTop1SVM
+        #from topk.svm import SmoothTop1SVM
+        from svm import SmoothTop1SVM
         loss_fn = SmoothTop1SVM(n_classes=args.n_classes)
         if device.type == 'cuda':
             loss_fn = loss_fn.cuda()
@@ -292,7 +295,8 @@ def test(datasets, cur, args):
             model_dict.update({'k_sample': args.B})
 
         if args.inst_loss == 'svm':
-            from topk.svm import SmoothTop1SVM
+            #from topk.svm import SmoothTop1SVM
+            from svm import SmoothTop1SVM
             instance_loss_fn = SmoothTop1SVM(n_classes=2)
             if device.type == 'cuda':
                 instance_loss_fn = instance_loss_fn.cuda()
