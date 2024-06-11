5aaeee0b9c941068717365f3312796cf1d93def4
pluto-the-planet <86825641+pluto-the-planet@users.noreply.github.com>
2024-06-11 17:47:45 +0500

    sp.py

--- utils/polynomial/sp.py
+++ utils/polynomial/sp.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torch.autograd as ag
 
-from topk.polynomial.divide_conquer import divide_and_conquer
-from topk.polynomial.multiplication import Multiplication
-from topk.polynomial.grad import d_logS_d_expX
+from utils.polynomial.divide_conquer import divide_and_conquer
+from utils.polynomial.multiplication import Multiplication
+from utils.polynomial.grad import d_logS_d_expX
 
 
 class LogSumExp(nn.Module):
