c71cded34a9c98fb2c6ba2c9b80f51495c1ecde5
pluto-the-planet <86825641+pluto-the-planet@users.noreply.github.com>
2024-06-12 22:45:08 +0500

    dataset_generic.py

--- datasets/dataset_generic.py
+++ datasets/dataset_generic.py
@@ -337,7 +337,7 @@ class Generic_MIL_Dataset(Generic_WSI_Classification_Dataset):
 		if not self.use_h5:
 			if self.data_dir:
 				print("data_dir",data_dir)
-				print("slide_id",slides_id)
+				print("slide_id",slide_id)
 				full_path = os.path.join(data_dir, 'pt_files', '{}.pt'.format(slide_id))
 				print("full_path",full_path)
 				if os.path.exists(full_path):
