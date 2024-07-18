a960e75884c0a17cd74b45850b5726ab8dc3ca48
pluto-the-planet <86825641+pluto-the-planet@users.noreply.github.com>
2024-07-18 12:17:10 +0500

    Update train_stage1.py

--- train_stage1.py
+++ train_stage1.py
@@ -50,7 +50,7 @@ def main(args):
         train_dataset, val_dataset, test_dataset = dataset.return_splits(from_id=False,
                 csv_path='{}/splits_{}.csv'.format(args.split_dir, i))
         datasets = (train_dataset, val_dataset, test_dataset)
-        results, test_auc, val_auc, test_f1, val_f1 = train(datasets, i, args)
+        results,test_acc,val_acc, test_auc, val_auc, test_f1, val_f1,test_auc_pk, val_auc_pk  = train(datasets, i, args)
         all_test_auc.append(test_auc)
         all_val_auc.append(val_auc)
         all_test_f1.append(test_f1)
